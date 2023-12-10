4919dc54839dfe5b0dc048eee61e6e7e3a3dc5cd
ABay23 <alejandrobay23@gmail.com>
2023-12-10 14:48:34 -0500

    New Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import am from './images/am.jpg'
 
 export default function Home() {
   return (
@@ -7,6 +8,17 @@ export default function Home() {
         <div>
           <h1>Home Page</h1>
         </div>
+        <section className='mb-8 relative'>
+          <div className='bg-gray-100 p-4 rounded-lg'>
+            <Image src={am} alt='burger' width={700} height={600}></Image>
+
+            <p className='text-lg font-semibold mt-8'>Section 1 Title</p>
+            <p className='mt-2 bg-teal-500'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </section>
       </div>
     </main>
   )
