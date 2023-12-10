435c78fb918d7286b3cfa76742685ab227b87468
ABay23 <alejandrobay23@gmail.com>
2023-12-10 15:14:11 -0500

    Updated Mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,24 @@ import am from './images/am.jpg'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+    <main className='flex min-w-full flex-col items-center justify-between p-10 w-screen'>
+      <div
+        className='z-10 w-full align-middle flex flex-col items-center p-2'
+        // 'z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'
+      >
         <div>
           <h1>Home Page</h1>
         </div>
         <section className='mb-8 relative'>
-          <div className='bg-gray-100 p-4 rounded-lg'>
+          <div className=' h-2/3 w-full bg-teal-400 p-4 rounded-lg'>
             <Image src={am} alt='burger' width={700} height={600}></Image>
 
-            <p className='text-lg font-semibold mt-8'>Section 1 Title</p>
+            <p className='text-lg font-semibold mt-8 bg-slate-600'>
+              {`Frank's Burger`}
+            </p>
             <p className='mt-2 bg-teal-500'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Get a FREE SODA if the total of your ticket is more than $30 at
+              the Seaside Cafe 🔥 .
             </p>
           </div>
         </section>
